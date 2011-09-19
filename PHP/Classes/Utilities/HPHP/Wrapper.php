58a5604f15d1377d122d618963b5906a0120499e
Christian <callowaylc@yahoo.com>
2011-09-19 09:38:03 -0700

    Starting with clean development branch; Initial commit for set of utilities classes for working with hiphop binaries

---
+++ PHP/Classes/Utilities/HPHP/Wrapper.php
@@ -0,0 +1,13 @@
+<?php
+namespace \eGloo\Utilities\HPHP;
+
+/**
+ * 
+ * A wrapper interface to hiphop compiled programs; haven't really thought this
+ * out yet
+ * @author Christian Calloway
+ *
+ */
+class Wrapper { 
+	
+}
