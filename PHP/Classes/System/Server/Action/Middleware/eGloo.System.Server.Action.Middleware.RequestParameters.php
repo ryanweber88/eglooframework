5342e6350bbb0c0f47431c4c7ea5330a138e8e6a
Christian <callowaylc@yahoo.com>
2011-10-07 08:44:47 -0700

    Middleware passing tests

---
+++ PHP/Classes/System/Server/Action/Middleware/eGloo.System.Server.Action.Middleware.RequestParameters.php
@@ -0,0 +1,27 @@
+<?php
+namespace eGloo\System\Server\Action\Middleware;
+
+use \eGloo\System\Server\Action\HTTP\Request;
+use \eGloo\System\Server\Action\HTTP\Response;
+
+/**
+ * 
+ * Responsible for setting PHP superglobal request variables from request object
+ * @author Christian Calloway
+ *
+ */
+class RequestParameters extends Middleware { 
+	
+	
+		
+	public function processRequest(Request &$request) { 
+
+		// point superglobals (which do not exist in cli environment) to $request object's
+		// parsed (from mongrel) values
+		$GLOBALS['_GET']     = &$request->GET;
+		$GLOBALS['_POST']    = &$request->POST;
+		$GLOBALS['_REQUEST'] = &$request->GET;
+		$GLOBALS['_COOKIE']  = &$request->COOKIE; 
+	}
+	
+}
