b49f0bbfe301264be7de6cde545fea7b73291400
Christian <callowaylc@yahoo.com>
2011-10-14 15:26:59 -0700

    replacing lost classes

--- PHP/Classes/DataProcessing/DDL/Entity/eGloo.DataProcessing.DDL.Entity.Entity.php
+++ PHP/Classes/DataProcessing/DDL/Entity/eGloo.DataProcessing.DDL.Entity.Entity.php
@@ -12,11 +12,17 @@ namespace eGloo\DataProcessing\DDL\Entity;
  */
 abstract class Entity extends \eGloo\Dialect\Object { 
 	
-	function __construct() { }
+	function __construct() { 
+		$this->init();
+	}
 	
+	/**
+	 * 
+	 * Provides initialization for entity bean
+	 */
 	public function init() { }
 	
-	// CRUD METHODS -------------------------------------------------------- */
+	// CRUD METHODS -------------------------------------------------------- //
 	// TODO determine if these should be class or instance variables
 	
 	public static function create() { }
@@ -24,7 +30,9 @@ abstract class Entity extends \eGloo\Dialect\Object {
 	public function update() { }
 	public function delete() { }
 	
-	// MAGIC --------------------------------------------------------------- */
+	// MAGIC --------------------------------------------------------------- //
+	// This sections primary concern is communicating with underlying
+	// data class
 	
 	public function __get($name) { }
 	public function __set($name, $value) { }
@@ -34,5 +42,7 @@ abstract class Entity extends \eGloo\Dialect\Object {
 		// call class object magic method
 		parent::__call($name, $arguments);
 	}
+	
+	// CALLBACK 
 	
 }
---
+++ PHP/Classes/DataProcessing/DDL/Entity/eGloo.DataProcessing.DDL.Entity.EntityInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace eGloo\DataProcessing\DDL\Entity\EntityInterface;
+
+use \eGloo\DataProcessing\DDL;
+
+/**
+ * 
+ * Provides standard bearer interface for Entity objects
+ * @author Christian Calloway
+ *
+ */
+interface EntityInterface { 
+	
+}
