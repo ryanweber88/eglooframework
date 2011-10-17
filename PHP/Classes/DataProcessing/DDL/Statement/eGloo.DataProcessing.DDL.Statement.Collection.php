f3afbb0c65add2f3bd20260da3f7b259fa7d1ad6
Christian <callowaylc@yahoo.com>
2011-10-17 16:48:37 -0700

    entity interface

---
+++ PHP/Classes/DataProcessing/DDL/Statement/eGloo.DataProcessing.DDL.Statement.Collection.php
@@ -0,0 +1,17 @@
+<?php
+namespace eGloo\DataProcessing\DDL\Statement;
+
+use eGloo\DataProcessing\DDL;
+
+/**
+ * 
+ * Responsible for retrieving collection of statements available to
+ * entity
+ * @author Christian Calloway
+ *
+ */
+class Collection extends \eGlooDPStatement { 
+	
+	// TODO should execute be overriden and passed entity + calling method?
+	
+}
