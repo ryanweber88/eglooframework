cfcb25b6aac11e897c2193e39459787a2b21d9bd
Gilbert Sewovoe-Ekoue <gilberts55@hotmail.com>
2011-10-26 21:33:29 -0400

    Initial Commit

---
+++ PHP/Classes/Plugins/Commerce/User/User.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Description of User
+ * 
+ * Representation of User Entity
+ * Hold CRUD functionalities and user related methods
+ *
+ * @author gilbertek
+ */
+class User {
+	//put your code here
+}
+
+?>
