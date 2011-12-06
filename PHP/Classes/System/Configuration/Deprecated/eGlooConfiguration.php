eb21627f31b30769fe20b7059ab898d5c5aeea31
Gilbert Ekoue <gilbert@petflow.com>
2011-12-05 19:59:50 -0500

    eg_config variable errorfix

--- PHP/Classes/System/Configuration/Deprecated/eGlooConfiguration.php
+++ PHP/Classes/System/Configuration/Deprecated/eGlooConfiguration.php
@@ -1103,9 +1103,9 @@ final class eGlooConfiguration {
 			// Grab our environment variables to determine which application and deployment to run
 
 			foreach (self::$system_configuration as $key => $value) {
-				self::$configuration_options[$key] = $value['value'];
-				
-				if ($value['override'] === 'true') {
+				self::$configuration_options[$key] = (isset ($value['value']) ? $value['value'] : '');
+
+				if (isset ($value['override']) && $value['override'] === 'true') {
 					self::$configuration_possible_options[$key] = $value;
 				}
 			}
