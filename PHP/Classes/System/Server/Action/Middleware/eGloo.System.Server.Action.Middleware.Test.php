42d6ae4aa859de99eeb21a07d22d9e9b374ea8d5
Christian <callowaylc@yahoo.com>
2012-01-16 11:17:02 -0800

    Added some notes on functionality in our Test Middleware for stateful system

--- PHP/Classes/System/Server/Action/Middleware/eGloo.System.Server.Action.Middleware.Test.php
+++ PHP/Classes/System/Server/Action/Middleware/eGloo.System.Server.Action.Middleware.Test.php
@@ -25,12 +25,92 @@ class Test extends Middleware {
 		$counter = 0;
 		$logger  = Server\Application::instance()->context()->retrieve('logger.test');
 		
-		$user = \eGloo\DataProcessing\DDL\Entity\Test\User::find(1); 
-		$r    = new Response();
-		$r->content = 'suckit';
-		return $r;
+
+		// INITIALIZERS
+		$user = new \eGloo\DataProcessing\DDL\Entity\Test\User();
+		$user->name = 'Christian';
+		$user->mail = 'callowaylc@gmail.com';
+		
+		// create a user with following initilization fields
+		$user = new \eGloo\DataProcessing\DDL\Entity\Test\User([
+			'name' => 'Christian',
+			'mail' => 'callowaylc@gmail.com'
+		])
+		
+		// initialize a user with a block as opposed to hash - this
+		// will be convenient for callbacks, or simply capturing scope
+		// from some period of program execution
+		\eGloo\DataProcessing\DDL\Entity\Test\User(function($user) {
+			$user->name = 'Christian';
+			$user->mail = 'asdf@adf.com';
+		});
 		
 		
+		// FINDERS
+		
+		// all users from database (not recommended)
+		$set = \eGloo\DataProcessing\DDL\Entity\Test\User::all();
+		
+		// all users in batch from database in batches (not implemented yet) - in later versions
+		// a batch size can be left to a default which will be implicitly determined by entity
+		// at runtime by inspecting # of records in table
+		$set = \eGloo\DataProcessing\DDL\Entity\Test::findEach(["size" => 500, "start" => 100]);
+		
+		// range find - this will find all users in the given primary key range and return a result set object
+		\eGloo\DataProcessing\DDL\Entity\Test\User::find('1..100')
+
+		
+		// find by primary/natural key, which is determined implicitly
+		\eGloo\DataProcessing\DDL\Entity\Test\User::find(1);
+		\eGloo\DataProcessing\DDL\Entity\Test\User::find(1,47, 53);
+		\eGloo\DataProcessing\DDL\Entity\Test\User::find([1,47, 53])->orderBy('-name')->limit(1);
+		
+		
+		// DYNAMIC FINDERS - these are routed through __call method and in reality are simply calling a statement
+		// file of the same name (find_by_name.sql)
+		$set = \eGloo\DataProcessing\DDL\Entity\Test\User::find_by_name('ian_1', 'ian_2', 'ian_3')->orderBy('+id');
+		$set = \eGloo\DataProcessing\DDL\Entity\Test\User::find_by_mail(['callowaylc@gmail.com', 'fuck@it.com']);
+		
+		
+		// ASSOCIATIONS - these are mapped to sql files as well; a Product association to User would be found in
+		// a file named 'find_product'; ALL relationships are accessed via ucfirst(entity name) and all "has_many"
+		// relationships will pluralize the Entity name
+		
+		// make note that its "Products" and not "Product"
+		\eGloo\DataProcessing\DDL\Entity\Test\User::find(1)->Products[0]->title; 
+		\eGloo\DataProcessing\DDL\Entity\Test\User::find(1, 2, 3)[0]->Products[1]->title;
+		\eGloo\DataProcessing\DDL\Entity\Test\User::find(1)->Products[0]->User[0]->name == \eGloo\DataProcessing\DDL\Entity\Test\User::find(1)->name;
+		
+		// SETS - any finder "asking" for more than 1 result, will always return a QuerySet object, regardless
+		// of whether the query itself returns 0 or 1 records
+		
+		// returns a User object because we are only asking for 1 possible record
+		\eGloo\DataProcessing\DDL\Entity\Test\User::find(1);
+		
+		// returns QuerySet object
+		$set = \eGloo\DataProcessing\DDL\Entity\Test\User::find('1..20');
+		$set = \eGloo\DataProcessing\DDL\Entity\Test\User::find(1,2,3);
+		$set = \eGloo\DataProcessing\DDL\Entity\Test\User::find_by_name('christian', 'gilbert') // all dynamic finders return sets
+				
+		// set implement array access as well as "ranges" for allowing limits/offsets
+		$set[0]; // A User instance
+		$set['1..5'] == $set->limit(5)->offset(1);
+		$set->orderBy('+field'); // ascending order
+		$set->orderBy('-field'); // descending order
+		
+		// LAZY LOAD EVALUATION - evaluation (a database hit) will not occur until we have "officially" asked
+		// for data (see below)
+		
+		$users = \eGloo\DataProcessing\DDL\Entity\Test\User::find('1..100');
+		$users->orderBy('-name');
+		$users->limit(20)->offset(10) || $users['10..20'];
+		
+		// database hit
+		$users->count() || $users[0]->title || $users[0]->Products[0];
+		
+		
+		
+		/*
 		
 		
 		//$user->mail = 'superperrito@sucks.com';
@@ -50,7 +130,7 @@ class Test extends Middleware {
 		foreach (\eGloo\DataProcessing\DDL\Entity\Test\User::all() as $user) {
 			
 		}
-		*/
+		
 	
 		$set = \eGloo\DataProcessing\DDL\Entity\Test\User::all();
 		//$set->limit(5);
@@ -113,7 +193,7 @@ class Test extends Middleware {
 		echo "\ntime elapsed : " . (time() - $start) . "\n";
 		
 		$logger->log(ob_get_clean());
-		
+		*/
 		//exit ('Middleware\\Test::processRequest');
 		// return empty response, which will be basis for post processing
 		// of middleware components will begin
