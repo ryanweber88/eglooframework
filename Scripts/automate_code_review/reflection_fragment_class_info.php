1244242d37522e4e7700ed9cb9388842caba1b09
Christian <callowaylc@yahoo.com>
2012-01-16 09:18:14 -0800

    Moved automate scripts out of eglooframework

--- Scripts/automate_code_review/reflection_fragment_class_info.php
+++
@@ -1,7 +0,0 @@
-name,has_doc,line_start,line_end
-<?php
-$hasDoc = strlen($reflection_class->getDocComment()) 
-	? 'true'
-	: 'false';
-	
-echo "{$reflection_class->getName()},$hasDoc,{$reflection_class->getStartLine()},{$reflection_class->getEndLine()}";
--- Scripts/automate_code_review/reflection_fragment_file_info.php
+++
@@ -1,7 +0,0 @@
-path,has_doc,line_start,line_end
-<?php
-$hasDoc = strlen($reflection_file->getDocComment()) 
-	? 'true'
-	: 'false';
-
-echo "{$reflection_file->getFileName()},$hasDoc,0,{$reflection_file->getEndLine()}";
