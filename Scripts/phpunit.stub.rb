ed57924f29c7763fbe10b2ee34c0cd7c50b44ab6
Christian <callowaylc@yahoo.com>
2012-02-09 16:30:15 -0800

    updated phpunit.stub so it um, actually works

--- Scripts/phpunit.stub.rb
+++ Scripts/phpunit.stub.rb
@@ -9,7 +9,7 @@ require 'erb'
 require 'fileutils'
 
 # CONSTANTS
-
+DirectoryErb = File.dirname(__FILE__) + '/' + File.basename(__FILE__) + '.erb'
 
 # PATCHES
 
@@ -27,6 +27,10 @@ raise 'Specify file' if ARGV.empty?
 # test file's validity
 raise 'Invalid file' unless File.file?(file = ARGV[0])
 
+# set file to absolute path - this is needed to make a determination as
+# to where to put stub files
+file = File.absolute_path(file)
+
 
 # create file instance in block context
 File.open(file) do | file |
@@ -37,21 +41,29 @@ File.open(file) do | file |
   # get namepsace if available
   namespace = content.match(/^namespace\s+(\S+)/i)[1]
 
+ 
   # get class name if available
-  unless class_name = content.match(/^class\s+(\S+)/i)[1]
-    raise 'File does not contain valid class definition'
+  begin
+    unless class_name = content.match(/^(abstract|final)\s+class\s+(\S+)/i)[2]
+      raise 'File does not contain valid class definition'
+    end
+  ensure
+    raise 'Problem matching against class defintion - take a look at source' if class_name.nil?
+  
   end
+  
   
   # get stub path based on location and within what context it exists (framework, client, admin, etc)
   stub_directory  = File.dirname(File.absolute_path(file).match(/(^.+?)(Common|eglooframework|Administration.gloo|Client.gloo)/i)[0] + 
                     "/Test/PHPUnit/" +
                     File.absolute_path(file).match(/(Common|eglooframework|Administration.gloo|Client.gloo)\/(.+)/i)[2])
-  
+
+
   # create path to directory if it does not exist
   FileUtils.mkdir_p(stub_directory)
     
   # finally write stub template to directory
-  IO.write("#{stub_directory}/#{class_name}Test.php", ERB.new(IO.read('./phpunit.stub/stub.erb')).result(binding))
+  IO.write("#{stub_directory}/#{class_name}Test.php", ERB.new(IO.read("#{DirectoryErb}/stub.erb")).result(binding))
   
   puts "Rote #{stub_directory}/#{class_name}Test.php"
 end
