a8215d0f066ccef5c3a8d3d5820e8dac343cd547
Christian <callowaylc@yahoo.com>
2012-02-13 15:21:34 -0800

    sigh, forgot to add the fucking clean_template script

---
+++ Scripts/clean_template.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+# Removes property messages to object receiver - ie, converts to associative array
+
+# REQUIRES 
+
+require 'erb'
+require 'fileutils'
+
+# GO
+
+# raise exceptions if an argument does not exist, or if it is not valid file
+raise 'You must pass a valid file argument' if ARGV.empty? 
+raise 'File argument is invalid'            unless File.file? (file = ARGV[0])
+
+
+# open file and look for php instance patterns
+content = nil
+
+File.open(file) do | file |
+  content = file.read.gsub(/\$([a-zA-Z_0-9]+)\-\>([a-zA-Z_0-9]+)(\(.*?\))?(\-\>([A-Za-z0-9_]+))?/) do | match |
+    object, key, multi = $1, $2, $5
+
+    replace = "$#{object}['#{key.sub(/^get/, '')}']"
+    replace += "['#{multi}']" unless multi.nil?
+  
+    replace  
+  end
+end
+
+# make a copy/backup of file
+path = File.dirname file
+
+FileUtils.copy file, "#{path}/#{File.basename(file)}.backup"
+
+# overwrite target
+IO.write file, content
