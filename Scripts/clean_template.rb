5b7fd42445cfd34a4e26f040919fa5562d87fe46
Christian <callowaylc@yahoo.com>
2012-02-13 15:35:30 -0800

    fixed bug on clean_template

--- Scripts/clean_template.rb
+++ Scripts/clean_template.rb
@@ -10,8 +10,11 @@ require 'fileutils'
 # GO
 
 # raise exceptions if an argument does not exist, or if it is not valid file
-raise 'You must pass a valid file argument' if ARGV.empty? 
-raise 'File argument is invalid'            unless File.file? (file = ARGV[0])
+raise 'You must pass a valid file argument'                    if ARGV.empty? 
+raise 'File argument is invalid'                               unless File.file? (file = ARGV[0])
+raise 'You\'ve already cleaned this template or remove backup' if File.file? ("#{File.dirname(file)}/#{File.basename(file)}.backup")
+
+
 
 
 # open file and look for php instance patterns
@@ -21,12 +24,14 @@ File.open(file) do | file |
   content = file.read.gsub(/\$([a-zA-Z_0-9]+)\-\>([a-zA-Z_0-9]+)(\(.*?\))?(\-\>([A-Za-z0-9_]+))?/) do | match |
     object, key, multi = $1, $2, $5
 
-    replace = "$#{object}['#{key.sub(/^get/, '')}']"
-    replace += "['#{multi}']" unless multi.nil?
+    replace =  "$#{object}.#{key.sub /^get/, ''}"
+    replace += ".#{multi}" unless multi.nil?
+
   
     replace  
   end
 end
+
 
 # make a copy/backup of file
 path = File.dirname file
