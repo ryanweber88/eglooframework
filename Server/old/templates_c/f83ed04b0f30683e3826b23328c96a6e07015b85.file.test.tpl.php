e999191ef1bcd4df127d1ad1d4a7f1b308e778b3
zalgo <zalgo@ub12.(none)>
2012-07-24 16:35:47 -0400

    added appserver files; didnt do corectly because i am slo

---
+++ Server/old/templates_c/f83ed04b0f30683e3826b23328c96a6e07015b85.file.test.tpl.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version Smarty 3.1.0, created on 2011-09-20 11:41:12
+         compiled from "/tmp/test.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:9260604884e78de48eb6220-17088028%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'f83ed04b0f30683e3826b23328c96a6e07015b85' => 
+    array (
+      0 => '/tmp/test.tpl',
+      1 => 1316542900,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '9260604884e78de48eb6220-17088028',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty 3.1.0',
+  'unifunc' => 'content_4e78de48ebca7',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_4e78de48ebca7')) {function content_4e78de48ebca7($_smarty_tpl) {?><!---->     
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	    <title>eGloo | Skeleton Application</title>
+	</head>
+    <body>
+		<h1>eGloo Skeleton Application!</h1>
+    </body>
+</html>
+<?php }} ?>
