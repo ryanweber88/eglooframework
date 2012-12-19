7e2fbf42eaa395384f1b75e890ae3cbef0d4b84c
George Cooper <egd0000@egloo.com>
2012-12-19 02:00:38 -0500

    Fix

--- PHP/Classes/DataProcessing/DDL/Entity/Definition/eGloo.DataProcessing.DDL.Entity.Definition.Factory.php
+++ PHP/Classes/DataProcessing/DDL/Entity/Definition/eGloo.DataProcessing.DDL.Entity.Definition.Factory.php
@@ -4,27 +4,27 @@ namespace eGloo\DataProcessing\DDL\Entity\Definition;
 use \eGloo\DataProcessing\DDL;
 
 /**
- * 
+ *
  * Provides factory method for entity definitions
  * @author Christian Calloway
  * @deprecated Unless factory complexity increases, I don't see a reason
- * to house within its own class 
+ * to house within its own class
  *
  */
-class Factory extends \eGloo\Dialect\Object { 
-	
-	static public function &factory(Entity $entity) { 
-		
+class Factory extends \eGloo\Dialect\Object {
+
+	static public function &factory(Entity $entity) {
+
 		$key = $entity->_class->name;
-	
-		if (!isset(static::$definitions[$key])) { 
+
+		if (!isset(static::$definitions[$key])) {
 			$builder = new Builder($entity);
-			
+
 			static::$definitions[$key] = $builder->build();
 		}
-		
+
 		return static::$definitions[$key];
-	}	
-	
-	final private static $definitions = [ ];
+	}
+
+	private static $definitions = [ ];
 }
