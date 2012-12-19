40dd8411887c557512cc2989455a0449197642d0
George Cooper <egd0000@egloo.com>
2012-12-19 16:24:10 -0500

    fix

--- Scripts/git/hooks/modules/PHPCodeSniffer.php
+++ Scripts/git/hooks/modules/PHPCodeSniffer.php
@@ -59,9 +59,13 @@ class PHPCodeSniffer extends Hook\Module {
 			// now log all summarized results
 			if (count($file_matches)) {
 				$counter = 0;
-print_r($file_matches);
+
 				foreach($file_matches as $file_match) {
-					$error_match = $error_matches[$counter++];
+					if ( isset($error_matches[$counter++]) ) {
+						$error_match = $error_matches[$counter++];
+					} else {
+						continue;
+					}
 
 					$this->log(
 						"File '{$file_match[1]}' had the following errors:\n" .
