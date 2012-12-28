5f99cb13db76f9826757b1a023b0568e1ee5cc21
George Cooper <egd0000@egloo.com>
2012-12-28 10:08:53 -0500

    Fixing pdepend in build

--- PHP/Classes/DataProcessing/DDL/Transaction/eGloo.DataProcessing.DDL.Transaction.Transaction.php
+++ PHP/Classes/DataProcessing/DDL/Transaction/eGloo.DataProcessing.DDL.Transaction.Transaction.php
@@ -4,20 +4,22 @@ namespace eGloo\DataProcessing\DDL\Transaction;
 use \eGloo\DataProcessing\DDL;
 
 /**
- * 
+ *
  * Responsible for transactions between entity and corresponding interface sql
  * @author Christian Calloway
  *
  */
-class Transaction extends \eGloo\Dialect\Object implements Transaction { 
-	
-	function __construct(DDL\Entity\Entity $entity, DDL\Statement\Statement $statement) { 
+class Transaction extends \eGloo\Dialect\Object {
+
+// implements Transaction { <--- pdepend breaks on this
+
+	function __construct(DDL\Entity\Entity $entity, DDL\Statement\Statement $statement) {
 		$this->entity = $entity;
 		$this->statement = $statement;
 	}
-	
+
 	public function commit() { }
-	
+
 	protected $entity;
 	protected $statement;
 }
